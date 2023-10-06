46078961b0a9774056a0ffad962a0113e22514f4
tharshman <77440048+tharshman@users.noreply.github.com>
2023-10-06 15:25:48 -0400

    Update life_expectency_calculator.py

--- src/services/life_expectency_calculator.py
+++ src/services/life_expectency_calculator.py
@@ -3,3 +3,8 @@ from models.user_info import UserInfo
 class LifeExpectencyCalculator:
     def __init__(self):
         self._expectency = int()
+
+    def add_expectencies(self, age, sex, age_of_retirement):
+        expectencies = UserInfo(age, sex, years_to_retirement)
+        self._expectency.append(expectencies)
+        
