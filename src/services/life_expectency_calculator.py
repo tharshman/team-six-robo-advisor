b15b74a7b2745fc20c3a3ae5270a10e1ffc01501
Vincent Bortone <vbortone@gmail.com>
2023-10-08 00:39:04 -0400

    feat: stub out methods

--- src/services/life_expectency_calculator.py
+++ src/services/life_expectency_calculator.py
@@ -8,3 +8,11 @@ class LifeExpectancyCalculator:
     def add_expectancies(self, age, sex, age_of_retirement):
         expectancies = UserInfo(age, sex, age_of_retirement)
         self._expectancy.append(expectancies)
+
+    def calculate_for_user(self, user_info: UserInfo) -> UserInfo:
+        # TODO: perform calculation with actuary data, append it to user_info and return user_info object
+        return user_info
+
+    def __load_actuary_data(self):
+        # TODO: load actuary data
+        pass
