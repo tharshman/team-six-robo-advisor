cc0ce54918a9f661bb415452adbcf1c6bfae265d
Vincent Bortone <vbortone@gmail.com>
2023-10-09 23:15:59 -0400

    fix: add check for invalid sex

--- src/services/life_expectancy_calculator.py
+++ src/services/life_expectancy_calculator.py
@@ -11,20 +11,22 @@ class LifeExpectancyCalculator:
 
     def calculate_for_user(self) -> UserInfo:
         for row in self._actuary_table:
-            if int(row['Age']) == self._user_info.age:
-                if self._user_info == SexAtBirth.MALE:
-                    self._user_info.age_of_life_expectancy = int(row['Male'])
+            if int(row["Age"]) == self._user_info.age:
+                if self._user_info.sex_at_birth == SexAtBirth.MALE:
+                    self._user_info.life_expectancy = int(float(row["Male"]))
+                elif self._user_info.sex_at_birth == SexAtBirth.FEMALE:
+                    self._user_info.life_expectancy = int(float(row["Female"]))
                 else:
-                    self._user_info.age_of_life_expectancy = int(row['Female'])
+                    raise ValueError(f"No data available for sex {self._user_info.sex_at_birth}")
                 break
 
-        if self._user_info.age_of_life_expectancy == 0:
+        if self._user_info.life_expectancy == 0:
             raise ValueError(f"No data available for age {self._user_info.age}")
 
         return self._user_info
 
     def __load_actuary_data(self) -> list:
-        with open(self.LIFE_EXPECTANCY_DATA_FILE, 'r') as file:
+        with open(self.LIFE_EXPECTANCY_DATA_FILE, "r") as file:
             reader = csv.DictReader(file)
             table = list(reader)
         return table
