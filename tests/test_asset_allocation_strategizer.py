340ae97d9e49a06182af737cbf95311bd3c92bf8
Vincent Bortone <vbortone@gmail.com>
2023-10-09 23:38:51 -0400

    feat: update tests

--- tests/test_asset_allocation_strategizer.py
+++ tests/test_asset_allocation_strategizer.py
@@ -11,11 +11,11 @@ class TestAssetAllocationStrategizer(TestCase):
         user = UserInfo("Vin", 50, SexAtBirth.MALE, 70, 78)
         sut = AssetAllocationStrategizer(user)
         table = sut.calculate_for_user()
-        self.assertEqual(8, table.get_allocation_count())
+        self.assertEqual(8, len(table))
 
     def test_with_odd_ages(self):
         user = UserInfo("Vin", 54, SexAtBirth.MALE, 73, 78)
         sut = AssetAllocationStrategizer(user)
         table = sut.calculate_for_user()
-        self.assertEqual(7, table.get_allocation_count())
+        self.assertEqual(7, len(table))
 
--- tests/test_asset_allocation_table.py
+++ tests/test_asset_allocation_table.py
@@ -6,12 +6,15 @@ class TestAssetAllocationTable(TestCase):
     def test_add_allocation(self):
         table = AssetAllocationTable()
         table.add_allocation(10, 50, 50)
-        self.assertEqual(table.get_allocation_count(), 1)
+        self.assertEqual(len(table), 1)
 
-    def test_get_allocation_count(self):
+    def test_len(self):
         table = AssetAllocationTable()
-        self.assertEqual(table.get_allocation_count(), 0)
+        self.assertEqual(len(table), 0)
 
-    def test_convert_to_string(self):
+    def test_iteration(self):
         table = AssetAllocationTable()
-        self.assertEqual(str(table), "test")
+        table.add_allocation(10, 50, 50)
+        self.assertEqual(len(table), 1)
+        for row in table:
+            print(f"YTR: {row.years_to_retirement}   Stock: {row.percent_in_stock}   Bonds: {row.percent_in_bonds}")
