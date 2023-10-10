c7b60b7be453021fb24fa19a5eb9eecdde81fbb7
Vincent Bortone <vbortone@gmail.com>
2023-10-09 21:13:09 -0400

    feat: merge calculator code

--- src/services/life_expectancy_calculator.py
+++ src/services/life_expectancy_calculator.py
@@ -1,43 +1,26 @@
-from models import UserInfo
+import csv
+from models import UserInfo, SexAtBirth
 
 
 class LifeExpectancyCalculator:
-    def __init__(self, user_info: UserInfo):
-        self._expectancy = int()
-        self._user_info = user_info
+    LIFE_EXPECTANCY_DATA_FILE = "life_expectancy_db.csv"
 
-    def add_expectancies(self, age, sex, age_of_retirement):
-        expectancies = UserInfo(age, sex, age_of_retirement)
-        self._expectancy.append(expectancies)
+    def __init__(self, user_info: UserInfo):
+        self._user_info = user_info
+        self._actuary_table = self.__load_actuary_data()
 
     def calculate_for_user(self) -> UserInfo:
-            for row in table:
-                if int(row['Age']) == age:
-                    if gender == 'Male':
-                        return float(row['Male'])
-                    else:
-                        return float(row['Female'])
-            return self._user_info
+        for row in self._actuary_table:
+            if int(row['Age']) == self._user_info.age:
+                if self._user_info == SexAtBirth.MALE:
+                    self._user_info.age_of_life_expectancy = int(row['Male'])
+                else:
+                    self._user_info.age_of_life_expectancy = int(row['Female'])
+                break
+        return self._user_info
 
-    def load_actuary_data(self):
-        with open(filename, 'r') as file:
+    def __load_actuary_data(self) -> list:
+        with open(self.LIFE_EXPECTANCY_DATA_FILE, 'r') as file:
             reader = csv.DictReader(file)
             table = list(reader)
         return table
-        
-    def main():
-        filename = "Life Expectancy Male and Female 30 - 100.csv"
-        table = load_actuary_data(filename)
-
-        age = int(input("Enter age: "))
-        gender = input("Enter sex at birth (Male/Female): ").capitalize()
-
-        life_expectancy = calculate_for_user(age, gender, table)
-
-        if life_expectancy is not None:
-            print(f"Estimated life expectancy for a {gender} of age {age} is {life_expectancy} years.")
-        else:
-            print("Data not available for the given input.")
-
-    if __name__ == "__main__":
-        main()
